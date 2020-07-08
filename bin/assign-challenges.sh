d944387bf15b3d9badc42e2ae8a6bc9d7c7248d3
mirkoperillo <mirko.perillo@gmail.com>
2020-07-08 13:52:02 +0200

    improve assign script

--- bin/assign-challenges.sh
+++ bin/assign-challenges.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-java -jar ../target/challenges-jar-with-dependencies.jar Application --config $1 --url $2 --username $3 --password $4
+
+java -jar ${CHALLENGE_LIB_HOME:-..}/target/challenges-jar-with-dependencies.jar Application --config $1 --url $2 --username $3 --password $4
