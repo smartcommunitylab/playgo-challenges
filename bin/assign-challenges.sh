33ac465f6a746bfd8cdb6cfd233b4612c351ce54
mirkoperillo <mirko.perillo@gmail.com>
2020-08-12 14:09:21 +0200

    add execDate option to assign-challenges.sh

--- bin/assign-challenges.sh
+++ bin/assign-challenges.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-java -jar ${CHALLENGE_LIB_HOME:-..}/target/challenges-jar-with-dependencies.jar Application --config $1 --url $2 --username $3 --password $4 --assign ${5-""}
+java -jar ${CHALLENGE_LIB_HOME:-..}/target/challenges-jar-with-dependencies.jar Application --config $1 --url $2 --username $3 --password $4 --assign ${5-""} --execDate ${6-`date '+%Y-%M-%d'`}
